a0912e7ef6d19c0daa579d6a3cd3bbfb24b00d1c
Aleksanrd_m <avm.letters@gmail.com>
2022-10-12 19:15:48 +0300

    Занятие 4 все таски

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,50 @@
 function highlight(table) {
-  // ваш код...
+  for (const row of table.rows) {
+    
+    let cNum = getCellNumbers(table);
+    
+    if (row.parentElement == table.tHead) {
+      continue;
+    }
+    
+    switch (row.cells[cNum.Status].dataset.available) {
+      case undefined:
+        row.hidden = true;
+        break;
+      case 'false':
+        row.classList.add('unavailable');
+        break;
+      case 'true':
+        row.classList.add('available');
+        break;  
+    };
+ 
+    switch (row.cells[cNum.Gender].textContent) {
+      case 'm':
+        row.classList.toggle('male', true);
+        row.classList.toggle('female', false);
+        break;
+    
+        case 'f':
+          row.classList.toggle('male', false);
+          row.classList.toggle('female', true);
+          break;
+    }
+
+    if (Number(row.cells[cNum.Age].textContent)  < 18 ) {
+      row.style.textDecoration = 'line-through'; 
+    }
+  }
+}
+
+
+function getCellNumbers(table) {
+  return {
+    Name: 0,
+    Age: 1,
+    Gender: 2,
+    Status: 3
+  }
+
+  
 }
