c0b8ed8672d3233bde6f7ae6b949b03d8676ed93
Aleksanrd_m <avm.letters@gmail.com>
2022-10-23 12:42:22 +0300

    Модуль 6. Задания 1 и 2. Задание 5 с ошибкой временно убрал

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,41 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
+
   constructor(product) {
+
+    this.elem = createElement(`
+      <div class="card">
+      <div class="card__top">
+          <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+          <span class="card__price">€${product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+          <div class="card__title">${product.name}</div>
+          <button type="button" class="card__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+      </div>
+      </div>
+      `);
+
+    let btn1 = this.elem.querySelector('.card__button');
+
+    btn1.addEventListener('click', (event) => {
+
+      let pressEvent = new CustomEvent('product-add', {
+        detail: product.id,
+        bubbles: true, 
+      });
+
+      btn1.dispatchEvent(pressEvent);
+
+    });
+
+    btn1.addEventListener('product-add', event => {
+      console.log('product-add', event.detail);
+    });
+
+
   }
 }
